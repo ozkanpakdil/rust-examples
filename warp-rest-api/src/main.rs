de3195005e693ddd24b18a31c72b594eea14ed62
ozkanpakdil <ozkan.pakdil@gmail.com>
2022-03-10 22:08:34 +0000

    json serializer changed

--- warp-rest-api/src/main.rs
+++ warp-rest-api/src/main.rs
@@ -2,23 +2,23 @@ use chrono::Datelike;
 use serde_derive::{Deserialize, Serialize};
 
 use serde_json::json;
-use warp::{Filter};
+use warp::Filter;
 
 #[derive(Serialize, Deserialize)]
 struct ApplicationInfo {
     name: String,
-    year: u8,
+    year: i32,
 }
 
 #[tokio::main]
 async fn main() {
     let hello = warp::any().map(|| {
-        let ssss = chrono::Utc::now().date().year();
-        let app_info = json!({
-            "name":"rust",
-            "year": ssss
-        });
-        app_info.to_string()
+        let app_info = ApplicationInfo {
+            name: String::from("rust-warp"),
+            year: chrono::Utc::now().date().year(),
+        };
+
+        serde_json::to_string(&app_info).unwrap()
     });
 
     warp::serve(hello).run(([127, 0, 0, 1], 8080)).await;
