b1f9b1d064efe249d8c96132796637b7cb4f7bf6
ozkanpakdil <ozkan.pakdil@gmail.com>
2022-03-10 21:41:06 +0000

    actix rest responder added

---
+++ actix-rest-api/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use chrono::Datelike;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct AppInfo {
+    name: String,
+    year: i32,
+}
+
+async fn index() -> impl Responder {
+    let app_info = AppInfo {
+        name: String::from("rust-actix"),
+        year: chrono::Utc::now().date().year(),
+    };
+    HttpResponse::Ok().json(&app_info)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/hello", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
