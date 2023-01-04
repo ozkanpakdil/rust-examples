cdd06671a4092d068323e6f77ba67802d8baac87
ozkanpakdil <ozkan.pakdil@gmail.com>
2023-01-04 22:09:33 +0000

    using tor client for some requests.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,70 @@
 use arti_client::{BootstrapBehavior, TorClient};
-use futures::FutureExt;
+use job_scheduler::{Job, JobScheduler};
+use tor_rtcompat::PreferredRuntime;
 
 #[tokio::main]
 async fn main() {
-    test_arti().await;
-    println!("Hello, world!");
+    let mut tor_client = TorClient::builder()
+        .bootstrap_behavior(BootstrapBehavior::OnDemand)
+        .create_unbootstrapped()
+        .unwrap();
+
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new("0 0 * * * *".parse().unwrap(), || {
+        println!("I get executed every 1 hour!");
+        /*{
+            tor_client = TorClient::builder()
+                .bootstrap_behavior(BootstrapBehavior::OnDemand)
+                .create_unbootstrapped()
+                .unwrap();
+        }*/
+    }));
+
+
+    test_arti_http(&tor_client)
+        .await
+        .ok();
+    test_arti_whois(&tor_client)
+        .await
+        .ok();
 }
 
-async fn test_arti() -> Result<(), Box<dyn std::error::Error>> {
-    let tor_client = TorClient::builder()
-        .bootstrap_behavior(BootstrapBehavior::OnDemand)
-        .create_unbootstrapped();
+async fn test_arti_http(tor_client: &TorClient<PreferredRuntime>) -> Result<(), Box<dyn std::error::Error>> {
     // Initiate a connection over Tor to example.com, port 80.
-    let mut stream = tor_client.unwrap().connect(("api.ipify.org", 80)).await?;
+    let mut stream = tor_client.connect(("api.ipify.org", 80)).await?;
 
-
-    println!("1");
-    
     use futures::io::{AsyncReadExt, AsyncWriteExt};
-    
+
     // Write out an HTTP request.
     stream
-    .write_all(b"GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n")
-    .await?;
-    println!("2");
-    
+        .write_all(b"GET / HTTP/1.1\r\nHost: api.ipify.org\r\nConnection: close\r\n\r\n")
+        .await?;
+
     // IMPORTANT: Make sure the request was written.
     // Arti buffers data, so flushing the buffer is usually required.
-    stream.flush().await;
-    println!("3");
-    
+    stream.flush().await?;
+
     // Read and print the result.
     let mut buf = Vec::new();
     stream.read_to_end(&mut buf).await?;
-    println!("4");
 
-    println!("1- {}", String::from_utf8_lossy(&buf));
+    println!("{}", String::from_utf8_lossy(&buf));
+    Ok(())
+}
+
+async fn test_arti_whois(tor_client: &TorClient<PreferredRuntime>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut stream = tor_client.connect(("whois.apnic.net", 43)).await?;
+
+    use futures::io::{AsyncReadExt, AsyncWriteExt};
+
+    stream.write_all(b"1.2.3.4\r\n").await?;
+
+    stream.flush().await.ok();
+
+    let mut buf = Vec::new();
+    stream.read_to_end(&mut buf).await?;
+
+    println!("{}", String::from_utf8_lossy(&buf));
     Ok(())
 }
