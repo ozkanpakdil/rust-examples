25028bf9bcaa47d5043daf3d0cf7b553cbf3b822
Ozkan Pakdil <ozkan.pakdil@gmail.com>
2023-08-06 09:31:48 +0100

    https://stackoverflow.com/questions/76833702/there-was-a-problem-printing-text-using-piston-window

--- piston_render_text/src/main.rs
+++ piston_render_text/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use piston_window::{clear, Context, G2d, Glyphs, PistonWindow, Position, rectangle, text, TextureContext, TextureSettings, Transformed, WindowSettings};
+use piston_window::{clear, Glyphs, PistonWindow, rectangle, text, TextureContext, TextureSettings, Transformed, WindowSettings};
 use piston_window::types::Color;
 
 pub const TEXT_COLOR: Color = [1.0, 1.0, 1.0, 1.0];
@@ -36,10 +36,9 @@ pub fn main() {
             rectangle([1.0, 0.0, 0.0, 1.0], // red
                       [0.0, 0.0, 100.0, 100.0], // rectangle
                       ctx.transform, g);
-            // draw_text(&ctx, g, &mut glyphs, TEXT_COLOR, Pos { x: 0.0, y: 20.0 }, "301211");
             text::Text::new_color(TEXT_COLOR, 20)
                 .draw(
-                    "10iubuoiygbiuyg87tr85riuygiu1011123456789111",
+                    _args.get(1).unwrap_or(&String::from("10iubuoiygbiuyg87tr85riuygiu1011123456789111")),
                     &mut glyphs,
                     &ctx.draw_state,
                     ctx.transform.trans(0.0, 20.0),
@@ -49,28 +48,4 @@ pub fn main() {
             glyphs.factory.encoder.flush(device);
         });
     }
-}
-
-pub struct Pos {
-    pub x: f64,
-    pub y: f64,
-}
-
-pub fn draw_text(
-    ctx: &Context,
-    graphics: &mut G2d,
-    glyphs: &mut Glyphs,
-    color: Color,
-    pos: Pos,
-    text: &str,
-) {
-    text::Text::new_color(color, 20)
-        .draw(
-            text,
-            glyphs,
-            &ctx.draw_state,
-            ctx.transform.trans(pos.x as f64, pos.y as f64),
-            graphics,
-        )
-        .unwrap();
 }
