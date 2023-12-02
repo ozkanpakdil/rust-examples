77df1905f6531315bb9ff30824a7520d7d8cf416
ozkanpakdil <ozkan.pakdil@gmail.com>
2023-12-02 07:53:45 +0000

    rust rest services upgraded to latest versions

--- axum-rest-api/src/main.rs
+++ axum-rest-api/src/main.rs
@@ -22,8 +22,7 @@ async fn main() {
     let app = Router::new()
     .route("/hello", get(hello));
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
